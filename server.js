808b0d40f548bd5d2afa7b2e1b1d9b1c1c531bee
phurinaix <phurinaix@gmail.com>
2018-09-17 04:30:28 +0700

    update views engine

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const express = require('express');
 const hbs = require('hbs');
 const app = express();
 
-// app.use(express.json());
-// app.use(express.urlencoded());
+app.set('view engine', 'hbs');
+app.use(express.json());
+app.use(express.urlencoded());
 
 const accessToken = 'yQ2kd1osEoogk01nfEhrYyMy9DsDMF3Wnb6jiCJTK2QPuv3kV3qrX7X4vsT51sv43sKUI0UOdRhcVykTf5NGYF1eEei8Nl+nXZV8tIZ1iNlYiAjMrSKNRt3pREOkbWpWgiYDS/n0StZg4C7AdII3lgdB04t89/1O/w1cDnyilFU=';
 
 app.get("/", (req, res) => {
-    res.send('This is homepage');
+    res.render('home.hbs');
 });
 
 app.get("/bot", (req, res) => {
