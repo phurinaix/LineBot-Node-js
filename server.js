c78477e06b47c71faad5f990b00c6a272f4d8ff9
phurinaix <phurinaix@gmail.com>
2018-09-17 04:27:43 +0700

    edit server file

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require('express');
 const hbs = require('hbs');
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded());
+// app.use(express.json());
+// app.use(express.urlencoded());
 
 const accessToken = 'yQ2kd1osEoogk01nfEhrYyMy9DsDMF3Wnb6jiCJTK2QPuv3kV3qrX7X4vsT51sv43sKUI0UOdRhcVykTf5NGYF1eEei8Nl+nXZV8tIZ1iNlYiAjMrSKNRt3pREOkbWpWgiYDS/n0StZg4C7AdII3lgdB04t89/1O/w1cDnyilFU=';
 
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/bot", (req, res) => {
-    res.send(req.body);
+    // res.send(req.body);
 });
 
 app.listen(8080, () => {
