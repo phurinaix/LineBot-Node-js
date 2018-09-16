94c4147150a895ab310d2fd059dcba1cd006a380
phurinaix <phurinaix@gmail.com>
2018-09-17 05:02:05 +0700

    update events

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
 app.get("/bot", (req, res) => {
     var events = JSON.parse(req.body);
     res.send(events);
-    res.send(events[0].source.userId);
+    // res.send(events[0].source.userId);
 });
 
 app.listen(port, () => {
