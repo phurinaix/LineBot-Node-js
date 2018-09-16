5b03586d6a0583ad75f8627daec6f6447c2c962f
phurinaix <phurinaix@gmail.com>
2018-09-17 04:51:33 +0700

    re-order set and use

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const hbs = require('hbs');
 const app = express();
 const bodyParser = require('body-parser');
-app.use(bodyParser);
 
 app.set('view engine', 'hbs');
+app.use(bodyParser);
 
 const port = process.env.PORT || 3000;
 // const accessToken = 'yQ2kd1osEoogk01nfEhrYyMy9DsDMF3Wnb6jiCJTK2QPuv3kV3qrX7X4vsT51sv43sKUI0UOdRhcVykTf5NGYF1eEei8Nl+nXZV8tIZ1iNlYiAjMrSKNRt3pREOkbWpWgiYDS/n0StZg4C7AdII3lgdB04t89/1O/w1cDnyilFU=';
