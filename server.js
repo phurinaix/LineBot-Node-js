617d76ccf004393f3aedfddd33468f4b3b89e584
phurinaix <phurinaix@gmail.com>
2018-09-17 04:20:54 +0700

    update file

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ const express = require('express');
 const hbs = require('hbs');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded());
+
+const accessToken = 'yQ2kd1osEoogk01nfEhrYyMy9DsDMF3Wnb6jiCJTK2QPuv3kV3qrX7X4vsT51sv43sKUI0UOdRhcVykTf5NGYF1eEei8Nl+nXZV8tIZ1iNlYiAjMrSKNRt3pREOkbWpWgiYDS/n0StZg4C7AdII3lgdB04t89/1O/w1cDnyilFU=';
+
 app.get("/", (req, res) => {
+    res.send('This is homepage');
+});
+
+app.get("/bot", (req, res) => {
 
 });
 
