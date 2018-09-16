a2ac518ec510d0fc3dffe06f6b5e7130fbc31c78
phurinaix <phurinaix@gmail.com>
2018-09-17 04:55:02 +0700

    update bodyParser

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express();
 const bodyParser = require('body-parser');
 
 app.set('view engine', 'hbs');
-app.use(bodyParser);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 3000;
 // const accessToken = 'yQ2kd1osEoogk01nfEhrYyMy9DsDMF3Wnb6jiCJTK2QPuv3kV3qrX7X4vsT51sv43sKUI0UOdRhcVykTf5NGYF1eEei8Nl+nXZV8tIZ1iNlYiAjMrSKNRt3pREOkbWpWgiYDS/n0StZg4C7AdII3lgdB04t89/1O/w1cDnyilFU=';
