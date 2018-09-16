185637e1725a2951c7f86b59a980e90a71a09f65
phurinaix <phurinaix@gmail.com>
2018-09-17 05:26:07 +0700

    update request

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 3000;
-// const accessToken = 'yQ2kd1osEoogk01nfEhrYyMy9DsDMF3Wnb6jiCJTK2QPuv3kV3qrX7X4vsT51sv43sKUI0UOdRhcVykTf5NGYF1eEei8Nl+nXZV8tIZ1iNlYiAjMrSKNRt3pREOkbWpWgiYDS/n0StZg4C7AdII3lgdB04t89/1O/w1cDnyilFU=';
+const accessToken = 'yQ2kd1osEoogk01nfEhrYyMy9DsDMF3Wnb6jiCJTK2QPuv3kV3qrX7X4vsT51sv43sKUI0UOdRhcVykTf5NGYF1eEei8Nl+nXZV8tIZ1iNlYiAjMrSKNRt3pREOkbWpWgiYDS/n0StZg4C7AdII3lgdB04t89/1O/w1cDnyilFU=';
 
 app.get("/", (req, res) => {
     res.render('home.hbs');
@@ -17,8 +17,27 @@ app.get("/", (req, res) => {
 
 app.get("/bot", (req, res) => {
     var events = req.body;
-    res.send(events);
-    res.send(events[0].source.userId);
+    var replyToken = events.replyToken;
+    var message = {
+        type: 'text',
+        text: 'haha'
+    }
+    var data = {
+        replyToken: replyToken,
+        message: message
+    }
+    request.post({url: 'https://api.line.me/v2/bot/message/push',
+                form: data,
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + accessToken
+                }
+            }, (err, response, body) => {
+        if (err) {
+            return res.send('Error');
+        }
+        res.send('success');
+    });
 });
 
 app.listen(port, () => {
