6538128228a78ea4096ff320e525ebe89058e863
phurinaix <phurinaix@gmail.com>
2018-09-17 04:41:57 +0700

    update port in server file

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const request = require('request');
+// const request = require('request');
 const express = require('express');
 const hbs = require('hbs');
 const app = express();
@@ -7,7 +7,8 @@ app.set('view engine', 'hbs');
 app.use(express.json());
 app.use(express.urlencoded());
 
-const accessToken = 'yQ2kd1osEoogk01nfEhrYyMy9DsDMF3Wnb6jiCJTK2QPuv3kV3qrX7X4vsT51sv43sKUI0UOdRhcVykTf5NGYF1eEei8Nl+nXZV8tIZ1iNlYiAjMrSKNRt3pREOkbWpWgiYDS/n0StZg4C7AdII3lgdB04t89/1O/w1cDnyilFU=';
+const port = process.env.PORT || 3000;
+// const accessToken = 'yQ2kd1osEoogk01nfEhrYyMy9DsDMF3Wnb6jiCJTK2QPuv3kV3qrX7X4vsT51sv43sKUI0UOdRhcVykTf5NGYF1eEei8Nl+nXZV8tIZ1iNlYiAjMrSKNRt3pREOkbWpWgiYDS/n0StZg4C7AdII3lgdB04t89/1O/w1cDnyilFU=';
 
 app.get("/", (req, res) => {
     res.render('home.hbs');
@@ -17,6 +18,6 @@ app.get("/bot", (req, res) => {
     // res.send(req.body);
 });
 
-app.listen(8080, () => {
+app.listen(port, () => {
     console.log('Starting port');
 });
