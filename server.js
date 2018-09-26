3e09607197967fa99f1e5872d1c1ef59ef5382e1
phurinaix <phurinaix@gmail.com>
2018-09-26 23:43:47 +0700

    update message

--- server.js
+++ server.js
@@ -15,8 +15,43 @@ app.get("/", (req, res) => {
 });
 
 app.post("/webhook", (req, res) => {
-    
+    var text = req.body.events[0].message.text
+    var sender = req.body.events[0].source.userId
+    var replyToken = req.body.events[0].replyToken
+    console.log(text, sender, replyToken)
+    console.log(typeof sender, typeof text)
+    // console.log(req.body.events[0])
+    if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
+        sendText(sender, text)
+    }
+    res.sendStatus(200)
 });
+
+function sendText (sender, text) {
+    let data = {
+      to: sender,
+      messages: [
+        {
+          type: 'text',
+          text: 'สวัสดีค่ะ เราเป็นผู้ช่วยปรึกษาด้านความรัก สำหรับหมามิ้น 💞'
+        }
+      ]
+    }
+    request({
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer key Api'
+      },
+      url: 'https://api.line.me/v2/bot/message/push',
+      method: 'POST',
+      body: data,
+      json: true
+    }, function (err, res, body) {
+      if (err) console.log('error')
+      if (res) console.log('success')
+      if (body) console.log(body)
+    })
+}
 
 app.listen(port, () => {
     console.log('Starting port');
