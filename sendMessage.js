179ab4401dcab93479817b88df5a76b70860faad
phurinaix <phurinaix@gmail.com>
2018-10-25 18:57:52 +0700

    add cron

--- sendMessage.js
+++ sendMessage.js
@@ -1,5 +1,6 @@
 const request = require('request');
 const accessToken = 'SHwO9NnQZl55uilTN/LEpU7msQnq+OjDSX0vg/EXMeSSEzzQw3qZh18QSy7bRRQLO5GGgj2/VxtgdVIND2v1G4EtMlTR3/91WdgohUMYUyrLco/VFcolW14aR3fpeI9zo6TkYLkZmuoTdoAA3eEhWgdB04t89/1O/w1cDnyilFU=';
+const CronJob = require('cron').CronJob;
 
 function sendText (sender, text) {
     let data = {
@@ -26,5 +27,9 @@ function sendText (sender, text) {
       if (body) console.log(body)
     })
 }
+
+new CronJob('* 20 * * * *', function () {
+  sendText('U8220df82988e1c41d1d716e856945290', 'Wake up!')
+}, null, true, 'Asia/Bangkok');
 
 module.exports.sendText = sendText;
