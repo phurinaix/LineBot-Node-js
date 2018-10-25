a295c49e33d046651431280b739d037a1d5752db
phurinaix <phurinaix@gmail.com>
2018-10-25 16:21:32 +0700

    add fine

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.post("/webhook", (req, res) => {
         sendMessage.sendText(sender, req.body.events[0]);
     }
     else if (text === 'fine') {
-        sendMessage.sendText(sender, 'what data ?');
+        sendMessage.sendText(sender, 'What the fuck bro');
     }
     res.sendStatus(200)
 });
