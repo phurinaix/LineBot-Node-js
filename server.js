d41d59ba1e2b47c9ca0287622a606c672b2c00c5
phurinaix <phurinaix@gmail.com>
2018-11-01 02:11:13 +0700

    add if else to check tolowercase

--- server.js
+++ server.js
@@ -47,64 +47,70 @@ app.post("/webhook", (req, res) => {
     }
     else if (type == 'message') {
         var sender = req.body.events[0].source.userId
-        var text = req.body.events[0].message.text.toLowerCase().replace(/\s+/g, "");
+        var text = req.body.events[0].message.text.replace(/\s+/g, "");
 
-        // greeting message
-        if (words.greeting.includes(text)) {
-            sendMessage(sender, 'สวัสดีจ้ะ');
+        if (typeof text === 'undefined') {
+            console.log("error");
         }
-
-        // mode 1
-        else if (words.mode_1.includes(text)) {
-            lotteryResult.then((result) => {
-                if (!result) {
-                    sendMessage(sender, 'ไม่มีค่า');
-                }
-                else {
-                    sendMessage(sender, result);
-                }
-            }).catch((err) => {
-                sendMessage(sender, "error");
-            });
-        }
-
-        // mode 2
-        else if (words.mode_2.includes(text)) {
-            asyncTime.then((result) => {
-                sendMessage(sender, result);
-            }).catch((err) => {
-                sendMessage(sender, "error");
-            });
-        }
-
-        // mode 3
-        else if (words.mode_3.includes(text)) {
-            sendMessage(sender, 'โหมด 3');
-        }
-
-        // mode 4
-        else if (words.mode_4.includes(text)) {
-            sendMessage(sender, 'โหมด 4');
-        }
-
-        // mode 5
-        else if (words.mode_5.includes(text)) {
-            sendMessage(sender, 'โหมด 5');
-        }
-
-        // mode 6
-        else if (words.mode_6.includes(text)) {
-            sendMessage(sender, 'โหมด 6');
-        }
-
-        // mode 7
-        else if (words.mode_7.includes(text)) {
-            sendMessage(sender, 'โหมด 7');
-        }
-
-        // another word
         else {
-            sendMessage(sender, 'I don\'t know');
+            text = text.toLowerCase();
+            // greeting message
+            if (words.greeting.includes(text)) {
+                sendMessage(sender, 'สวัสดีจ้ะ');
+            }
+
+            // mode 1
+            else if (words.mode_1.includes(text)) {
+                lotteryResult.then((result) => {
+                    if (result == 'null' || result == 'undefined') {
+                        sendMessage(sender, 'ไม่มีค่า');
+                    }
+                    else {
+                        sendMessage(sender, result);
+                    }
+                }).catch((err) => {
+                    sendMessage(sender, "error");
+                });
+            }
+
+            // mode 2
+            else if (words.mode_2.includes(text)) {
+                asyncTime.then((result) => {
+                    sendMessage(sender, result);
+                }).catch((err) => {
+                    sendMessage(sender, "error");
+                });
+            }
+
+            // mode 3
+            else if (words.mode_3.includes(text)) {
+                sendMessage(sender, 'โหมด 3');
+            }
+
+            // mode 4
+            else if (words.mode_4.includes(text)) {
+                sendMessage(sender, 'โหมด 4');
+            }
+
+            // mode 5
+            else if (words.mode_5.includes(text)) {
+                sendMessage(sender, 'โหมด 5');
+            }
+
+            // mode 6
+            else if (words.mode_6.includes(text)) {
+                sendMessage(sender, 'โหมด 6');
+            }
+
+            // mode 7
+            else if (words.mode_7.includes(text)) {
+                sendMessage(sender, 'โหมด 7');
+            }
+
+            // another word
+            else {
+                sendMessage(sender, 'I don\'t know');
+            }
         }
     }
     res.sendStatus(200)
@@ -114,13 +120,15 @@ app.listen(port, () => {
     console.log('Starting port');
 });
 
-lotteryResult.then((result) => {
-    // console.log(`length = ${res.length} and value is : ${res}`);
-    console.log(typeof result, result);
-}).catch((err) => {
-    console.log("error");
-});
+// lotteryResult.then((result) => {
+//     // console.log(`length = ${res.length} and value is : ${res}`);
+//     console.log(typeof result, result);
+// }).catch((err) => {
+//     console.log("error");
+// });
 
-asyncTime.then((result) => {
-    console.log(typeof result, result);
-});
+// asyncTime.then((result) => {
+//     console.log(typeof result, result);
+// });
+
+console.log(process.ENV);
